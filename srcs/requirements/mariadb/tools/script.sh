453ea637aaf75546f264c511ba10272af9e6d39f
najeuneh <najeuneh@Inception.myguest.virtualbox.org>
2025-06-11 19:12:22 +0200

    all

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-# Démarrer MariaDB en arrière-plan
+set -e
+
+DB_DIR="/var/lib/mysql/mysql"
+
 mysqld_safe &
 
-# Attendre que le serveur soit prêt
-while ! mysqladmin ping -u root --silent; do
+echo "Attente que MariaDB démarre..."
+until mysqladmin ping --silent; do
     sleep 1
 done
 
-# Créer la base de données et l'utilisateur avec les bonnes permissions
-mysql -u root <<EOF
+echo "MariaDB est prêt"
+
+
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<EOF
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
-CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'localhost';
+CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
 
+echo "Base créée et utilisateur configuré."
+
 mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
 
+echo "Lancement de MariaDB"
 exec mysqld_safe
